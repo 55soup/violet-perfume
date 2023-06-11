b3d5f79fb3e1125e8b3e62bbca04751cbf55958d
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-06-11 10:35:10 +0900

    ✨init repo

---
+++ perfume/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+def list_perfume(request):
+    # product_list = Product.objects.all() #DB에 있는 product 전체 가져오자
+    # context = {'product_list' : product_list} #product_list라는 키로 놓자
+    # #product/product_list.html에 보내자
+    # return render(request, 'product/product_list.html', context)
+    return render(request, 'perfume/index.html')
